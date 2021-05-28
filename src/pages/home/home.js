878ccb0ca302db5549e1e891d87cf9d8da8923de
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-05-28 10:22:46 +0100

    rollback

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,11 +1,14 @@
 import './home.css'
 import React from 'react';
+import {useHistory} from 'react-router-dom'
 
 const Home = () => {
-
+    const router = useHistory();
     return (
-        <div>
+        <div className={'home-main'}>
             <h1>Home</h1>
+
+            <h2 onClick={() => router.push('login')}>GO TO LOGIN</h2>
         </div>
     )
 }
--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,11 +1,13 @@
 import './login.css'
 import React from "react";
+import {useHistory} from 'react-router-dom'
 
 const Login = () => {
-
+    const router = useHistory()
     return (
         <div>
             <h1>Page login</h1>
+            <h2 onClick={() => router.push('home')}>GO TO HOME</h2>
         </div>
     )
 }
