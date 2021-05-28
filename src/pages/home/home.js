134f2b11170f70c862ab05bac50c37c77153e85a
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-05-28 11:55:39 +0100

    rollback

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,13 +1,20 @@
 import './home.css'
-import React from 'react';
+import React, {useCallback} from 'react';
 import {useHistory} from 'react-router-dom'
+import {useDispatch, useSelector,} from "react-redux";
+import {incrementerCounter} from '../../store/store'
 
 const Home = () => {
+    const dispatch = useDispatch();
+    const counter = useSelector((state) => state.counter.counter)
+    const increment = useCallback((counter) => dispatch(incrementerCounter(counter)), [dispatch]);
+
     const router = useHistory();
     return (
         <div className={'home-main'}>
             <h1>Home</h1>
-
+            <h2>CPT : {counter}</h2>
+            <h2 onClick={() => increment(counter + 1)}>Incrementer</h2>
             <h2 onClick={() => router.push('login')}>GO TO LOGIN</h2>
         </div>
     )
--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,12 +1,16 @@
 import './login.css'
 import React from "react";
 import {useHistory} from 'react-router-dom'
+import {useSelector} from "react-redux";
 
 const Login = () => {
-    const router = useHistory()
+    const router = useHistory();
+    const counter = useSelector((state) => state.counter.counter)
+
     return (
         <div>
             <h1>Page login</h1>
+            <h2>CPT : {counter}</h2>
             <h2 onClick={() => router.push('home')}>GO TO HOME</h2>
         </div>
     )
