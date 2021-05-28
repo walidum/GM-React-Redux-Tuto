8b0552f99209a8f591c8c159a157475704d3a688
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-05-28 11:57:57 +0100

    rollback

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,16 +1,20 @@
 import './login.css'
-import React from "react";
+import React, {useCallback} from "react";
 import {useHistory} from 'react-router-dom'
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {incrementerCounter} from "../../store/store";
 
 const Login = () => {
+    const dispatch = useDispatch();
     const router = useHistory();
     const counter = useSelector((state) => state.counter.counter)
+    const increment = useCallback((counter) => dispatch(incrementerCounter(counter)), [dispatch]);
 
     return (
         <div>
             <h1>Page login</h1>
             <h2>CPT : {counter}</h2>
+            <h2 onClick={() => increment(counter + 1)}>Incrementer</h2>
             <h2 onClick={() => router.push('home')}>GO TO HOME</h2>
         </div>
     )
