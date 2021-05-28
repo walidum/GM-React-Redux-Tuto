1acc72ea81506fbf930d6670cf04bc955b88771f
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-05-28 16:44:23 +0100

    rollback

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,21 +1,36 @@
 import './home.css'
-import React, {useCallback} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {useHistory} from 'react-router-dom'
 import {useDispatch, useSelector,} from "react-redux";
 import {incrementerCounter} from '../../store/store'
+import API from "../../api/api";
 
 const Home = () => {
     const dispatch = useDispatch();
     const counter = useSelector((state) => state.counter.counter)
     const increment = useCallback((counter) => dispatch(incrementerCounter(counter)), [dispatch]);
-
     const router = useHistory();
+
+    const [albums, setAlbums] = useState([]);
+    useEffect(() => {
+        API.get('albums')
+            .then(res => {
+                setAlbums(res.data);
+                console.log(albums)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    })
     return (
         <div className={'home-main'}>
             <h1>Home</h1>
             <h2>CPT : {counter}</h2>
             <h2 onClick={() => increment(counter + 1)}>Incrementer</h2>
             <h2 onClick={() => router.push('login')}>GO TO LOGIN</h2>
+            {albums && albums.map(album => (
+                <h4>{album.id} {album.title}</h4>
+            ))}
         </div>
     )
 }
