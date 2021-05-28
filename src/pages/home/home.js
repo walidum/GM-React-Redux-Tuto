f0fff08eedcb996e0059c95b7b3d5da51926e1e1
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-05-28 09:22:23 +0100

    rollback

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,23 +1,11 @@
 import './home.css'
-import {useDispatch, useSelector} from "react-redux";
-import React, {useCallback} from 'react';
-import {incrementCounter,setLoggedIn,setLoggedOut,addItem} from '../../store/store'
-import {Link} from "react-router-dom";
-const  Home = ()=>{
-    const dispatch = useDispatch();
-    const isLoggedIn = useSelector((state)=> state.counter.isLoggedIn)
-    const counter = useSelector((state)=>state.counter.counter)
-     const increment = useCallback((counter) =>
-            dispatch(incrementCounter(counter)), [dispatch]);
+import React from 'react';
 
-    return(
+const Home = () => {
+
+    return (
         <div>
-            <h1>Home Page</h1>
-            <h2> la valeur de isLoggedIn : {isLoggedIn? 'true':'false'}</h2>
-            <h2> la valeur de conteur : {counter}</h2>
-
-            <button onClick={()=>increment(counter+1)}>Increment conter</button>
-            <Link to="/login">Go Login</Link>
+            <h1>Home</h1>
         </div>
     )
 }
